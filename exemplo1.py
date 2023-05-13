2d44b9808a3011e87552decf867455118c59a098
Alexsandra Oliveira Pacheco <107128820+AlexsandraOP14@users.noreply.github.com>
2023-05-13 16:19:15 -0300

    Add files via upload

---
+++ exemplo1.py
@@ -0,0 +1,9 @@
+lista = []
+traco = ('_')
+for i in range (5):
+    nomes = input('Digite o nome de um amigo: ')
+    lista.append(nomes)
+for i in range (5):
+    print(traco * 50)
+    print('Obrigada por fazer parte da minha vida: ',lista[i])
+    print(traco * 50)

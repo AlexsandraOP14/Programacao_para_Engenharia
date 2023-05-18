c3a9910c9a66827b9d38ae3a37b7c7a90979113a
Alexsandra Oliveira Pacheco <107128820+AlexsandraOP14@users.noreply.github.com>
2023-05-17 21:36:13 -0300

    Modifiquei exercicio1

--- exercicio1.py
+++ exercicio1.py
@@ -1,16 +1,11 @@
-# O objetivo é pedir a nota de 3 alunos e depois mostrar a nota mais alta, a mais baixa,
-# a média geral das notas e depois ordenar em ordem crescente
+listaMes = []
+listaTem = []
+ano = {}
 
-lista = []
-
-for i in range(3):
-    nota = float(input('Digite a nota do Aluno {} :'.format(i+1)))
-    lista.append(nota)
-print('A maior nota foi: ', max(lista))
-print('A menor nota foi: ', min(lista))
-lista.sort(reverse = False)
-print('A ordem crescente: ', lista)
-
-
-
-##exercicio vagner
+for i in range(4):
+    mes = input('Digite o mês que você deseja adicionar a temperatura: ')
+    listaMes.append(mes)
+    temperatura = float(input('Digite a temperatura média: '))
+    listaTem.append(temperatura)
+    ano[listaMes[i]] = listaTem[i]
+print(ano)
